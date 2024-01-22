1ac6b65732f1a4be7bbf4833a8b6e5e646b7b2f3
kkang123 <85389685+kkang123@users.noreply.github.com>
2024-01-22 19:34:05 +0900

    Time: 40 ms (31.29%), Space: 16.5 MB (74.73%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -7,3 +7,15 @@ class Solution:
             
             self.dp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
             return self.dp[n]
+        
+        
+# 코드 최적화 / 해시테이블 대신 두 개의 변수값만 있으면 계산 가능
+
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n<3 :
+#             return n
+#         pre, cur = 1, 2
+#         for _ in range(n-2):
+#             pre, cur = cur, pre + cur
+#         return cur
